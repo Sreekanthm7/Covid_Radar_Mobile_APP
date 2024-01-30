39804bd93562a39af79eec989ee27c42d1d756c9
sreekanth <sreekanth.m@vonnue.com>
2024-01-30 11:36:11 +0530

    feat: create get started and login page

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,7 @@
+import BackArrow from '../svg/backArrow.svg';
+import Track from '../svg/track.svg';
+
+export const Common = {
+  BackArrow,
+  Track,
+};
--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -1,5 +1,10 @@
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+
 export type RootStackParamList = {
   HOME: undefined;
   GET_STARTED: undefined;
   LOGIN: undefined;
 };
+
+export type RootStackNavigationProps =
+  NativeStackNavigationProp<RootStackParamList>;
